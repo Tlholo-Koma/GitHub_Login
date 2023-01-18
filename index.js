342ead0e60c9db9ffa7c3d554e2c5a4ad195c018
Tlholo-Koma <tlholo@bbd.co.za>
2023-01-18 22:13:57 +0200

    Change to jQuery

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const url = "https://github.com"
 
 
-function loginWithGithub() {
-    console.log("Here")
-    fetch(url+"/login/oauth/authorize", {
-        method: "GET",
-        client_id: "d26698775db52875ec91"
-    }).then(
-        console.log("Redirected")
-    )
-}
+// function loginWithGithub() {
+//     console.log("Here")
+//     fetch(url+"/login/oauth/authorize", {
+//         method: "GET",
+//         client_id: "d26698775db52875ec91"
+//     }).then(
+//         console.log("Redirected")
+//     )
+
+    
+// }
+
+$(".btn").click(function() {
+    $.get(url+"login/oauth/authorize?client_id=d26698775db52875ec91", function (data) {
+        console.log(`${data}`)
+    })
+})
