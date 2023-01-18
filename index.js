36c31c1e19d3008c67200ec68e6fb97667a015cb
Tlholo Koma <122435130+Tlholo-Koma@users.noreply.github.com>
2023-01-18 12:04:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const url = "https://github.com"
 function connect() {
     fetch(url+"/login/oauth/authorize", {
         method: "GET",
+        client_id: ${{ secrets.CLIENT_ID }}
     }).then(
         console.log("Redirected")
     )
